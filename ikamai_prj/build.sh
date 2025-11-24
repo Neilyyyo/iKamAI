3285decbbb2d9454014ecaa25935674d704e5586
maro-hub88 <neilfranklin618@gmail.com>
2025-11-24 19:28:22 +0800

    add libenchant on build.sh

--- ikamai_prj/build.sh
+++ ikamai_prj/build.sh
@@ -1,1 +1,8 @@
+#!/usr/bin/env bash
+# build.sh
+
+# Install system dependencies for enchant
+apt-get update
+apt-get install -y enchant-2 libenchant-2-2
+
 pip install -r requirements.txt && python manage.py collectstatic --noinput
