add6bd1dc2d231e139e1c51970693c24bf167e4b
maro-hub88 <neilfranklin618@gmail.com>
2025-11-24 20:06:54 +0800

    add dockerfile

--- ikamai_prj/build.sh
+++ ikamai_prj/build.sh
@@ -1,13 +1,1 @@
-#!/usr/bin/env bash
-# build.sh
-
-# Install system dependencies for enchant
-apt-get update
-apt-get install -y enchant-2 libenchant-2-2 libenchant-2-dev
-
-# Install Python dependencies
-pip install -r requirements.txt
-
-# Run Django migrations and collect static files
-python manage.py migrate
-python manage.py collectstatic --noinput
+pip install -r requirements.txt && python manage.py collectstatic --noinput
