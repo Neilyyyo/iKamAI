d1e001f8e07ebf5d389639d30306e08c43d2b159
maro-hub88 <neilfranklin618@gmail.com>
2025-11-24 19:46:46 +0800

    add libenchant on build.sh again

--- ikamai_prj/build.sh
+++ ikamai_prj/build.sh
@@ -3,6 +3,11 @@
 
 # Install system dependencies for enchant
 apt-get update
-apt-get install -y enchant-2 libenchant-2-2
+apt-get install -y enchant-2 libenchant-2-2 libenchant-2-dev
 
-pip install -r requirements.txt && python manage.py collectstatic --noinput
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Run Django migrations and collect static files
+python manage.py migrate
+python manage.py collectstatic --noinput
