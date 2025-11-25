6dc6072688d30760c59911c294a835dc2e5d1dcb
maro-hub88 <neilfranklin618@gmail.com>
2025-11-25 19:19:53 +0800

    use tflite remove pttxyz and opencv

--- ikamai_prj/ikamai_app/predictor.py
+++ ikamai_prj/ikamai_app/predictor.py
@@ -1,27 +1,98 @@
 import cv2
 import numpy as np
-import tensorflow as tf  # CHANGED: Import TensorFlow for TFLite
-from cvzone.HandTrackingModule import HandDetector
 import threading
 from spellchecker import SpellChecker 
-import pyttsx3
 import math
 import base64
 import io
 from PIL import Image
+import mediapipe as mp
+
+# CHANGED: Use tflite_runtime
+try:
+    from tflite_runtime.interpreter import Interpreter
+except ImportError:
+    import tensorflow.lite as tflite
+    Interpreter = tflite.Interpreter
+
+# --- REPLACEMENT FOR CVZONE (Server Safe) ---
+# We define this class locally so we don't need to install 'cvzone' 
+# which forces the wrong OpenCV version.
+class HandDetector:
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, minTrackCon=0.5):
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.minTrackCon = minTrackCon
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(
+            static_image_mode=self.mode,
+            max_num_hands=self.maxHands,
+            min_detection_confidence=self.detectionCon,
+            min_tracking_confidence=self.minTrackCon
+        )
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self, img, draw=True, flipType=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        allHands = []
+        h, w, c = img.shape
+        
+        if self.results.multi_hand_landmarks:
+            for handType, handLms in zip(self.results.multi_handedness, self.results.multi_hand_landmarks):
+                myHand = {}
+                lmList = []
+                xList = []
+                yList = []
+                for id, lm in enumerate(handLms.landmark):
+                    px, py, pz = int(lm.x * w), int(lm.y * h), int(lm.z * w)
+                    lmList.append([px, py, pz])
+                    xList.append(px)
+                    yList.append(py)
+
+                xmin, xmax = min(xList), max(xList)
+                ymin, ymax = min(yList), max(yList)
+                boxW, boxH = xmax - xmin, ymax - ymin
+                bbox = xmin, ymin, boxW, boxH
+                cx, cy = bbox[0] + (bbox[2] // 2), bbox[1] + (bbox[3] // 2)
+
+                myHand["lmList"] = lmList
+                myHand["bbox"] = bbox
+                myHand["center"] = (cx, cy)
+                
+                # Flip logic to match cvzone behavior if needed
+                if flipType:
+                    if handType.classification[0].label == "Right":
+                        myHand["type"] = "Left"
+                    else:
+                        myHand["type"] = "Right"
+                else:
+                    myHand["type"] = handType.classification[0].label
+                
+                allHands.append(myHand)
+                
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+                    cv2.rectangle(img, (bbox[0] - 20, bbox[1] - 20),
+                                  (bbox[0] + bbox[2] + 20, bbox[1] + bbox[3] + 20),
+                                  (255, 0, 255), 2)
+        
+        return allHands, img
+# ---------------------------------------------
+
 
 class SignPredictor:
     def __init__(self):
-        # REMOVED: self.model = load_model('cnn8grps_rad1_model.tflite')
-        
         # CHANGED: Load TFLite Model
-        self.interpreter = tf.lite.Interpreter(model_path='cnn8grps_rad1_model.tflite')
+        self.interpreter = Interpreter(model_path='cnn8grps_rad1_model.tflite')
         self.interpreter.allocate_tensors()
         
-        # Get input and output details
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
         
+        # CHANGED: Use the local safe HandDetector
         self.detector = HandDetector(maxHands=1)
         self.lock = threading.Lock()
         
@@ -37,7 +108,10 @@ class SignPredictor:
         self.count = 0
         self.prev_char = ""
         self.current_symbol = ""
-        self.speak_engine = pyttsx3.init()
+        
+        # REMOVED: self.speak_engine = pyttsx3.init() 
+        # CAUSE OF CRASH: Servers have no audio drivers.
+
         self.prediction = ""
         self.suggestions = ["", "", "", ""]
         self.manual_override = False
@@ -45,7 +119,6 @@ class SignPredictor:
     def distance(self, x, y):
         return math.sqrt(((x[0] - y[0]) ** 2) + ((x[1] - y[1]) ** 2))
 
-    # --- KEEPING EXISTING HELPERS ---
     def action1(self):
         idx_space = self.str.rfind(" ")
         idx_word = self.str.find(self.word, idx_space)
@@ -67,8 +140,9 @@ class SignPredictor:
         self.str = self.str[:idx_word] + self.word4.upper()
 
     def speak_fun(self):
-        self.speak_engine.say(self.str)
-        self.speak_engine.runAndWait()
+        # CHANGED: Do nothing on server. 
+        # Return the text to the frontend if you want the browser to speak.
+        pass
 
     def clear_fun(self):
         self.str = ""
@@ -82,21 +156,14 @@ class SignPredictor:
                 'suggestions': [self.word1, self.word2, self.word3, self.word4]
             }
 
-    # --- Process frame from frontend ---
     def process_web_frame(self, image_data_base64):
         try:
-            # Decode base64 image
             img_str = image_data_base64.split(',')[1]
             decoded = base64.b64decode(img_str)
             image = Image.open(io.BytesIO(decoded))
-            
-            # Convert to numpy array (RGB)
             frame = np.array(image)
-            
-            # Convert RGB to BGR (OpenCV format)
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
             
-            # Run existing detection logic
             frame = cv2.flip(frame, 1)
             hands, img = self.detector.findHands(frame, flipType=False)
 
@@ -106,7 +173,6 @@ class SignPredictor:
                 x, y, w, h = hand['bbox']
                 offset = 29
                 
-                # Check bounds
                 if y-offset >= 0 and x-offset >= 0:
                     cropped = frame[y-offset:y+h+offset, x-offset:x+w+offset]
 
@@ -124,21 +190,13 @@ class SignPredictor:
 
     def predict(self, test_image):
         white = test_image
-        # TFLite expects strict types, ensure float32
         white = white.astype(np.float32)
         white = white.reshape(1, 400, 400, 3)
         
-        # CHANGED: TFLite Inference Logic
-        # 1. Set input tensor
         self.interpreter.set_tensor(self.input_details[0]['index'], white)
-        
-        # 2. Run inference
         self.interpreter.invoke()
-        
-        # 3. Get output tensor
         output_data = self.interpreter.get_tensor(self.output_details[0]['index'])
         
-        # Continue with your existing logic using the output
         prob = np.array(output_data[0], dtype='float32')
         
         ch1 = np.argmax(prob, axis=0)
@@ -150,7 +208,10 @@ class SignPredictor:
 
         pl = [ch1, ch2]
 
-        # condition for [Aemnst]
+        # --- LOGIC REMAINS EXACTLY THE SAME AS YOUR FILE BELOW THIS LINE ---
+        # (I have preserved your exact logic, just removing the cvzone dependency
+        # and audio dependency that causes the crash)
+
         l = [[5, 2], [5, 3], [3, 5], [3, 6], [3, 0], [3, 2], [6, 4], [6, 1], [6, 2], [6, 6], [6, 7], [6, 0], [6, 5],
              [4, 1], [1, 0], [1, 1], [6, 3], [1, 6], [5, 6], [5, 1], [4, 5], [1, 4], [1, 5], [2, 0], [2, 6], [4, 6],
              [1, 0], [5, 7], [1, 6], [6, 1], [7, 6], [2, 5], [7, 1], [5, 4], [7, 0], [7, 5], [7, 2]]
@@ -173,14 +234,12 @@ class SignPredictor:
                 0] and self.pts[0][0] > self.pts[20][0]) and self.pts[5][0] > self.pts[4][0]:
                 ch1 = 2
 
-        # condition for [c0][aemnst]
         l = [[6, 0], [6, 6], [6, 2]]
         pl = [ch1, ch2]
         if pl in l:
             if self.distance(self.pts[8], self.pts[16]) < 52:
                 ch1 = 2
 
-        # condition for [gh][bdfikruvw]
         l = [[1, 4], [1, 5], [1, 6], [1, 3], [1, 0]]
         pl = [ch1, ch2]
         if pl in l:
@@ -188,28 +247,24 @@ class SignPredictor:
                 0] and self.pts[0][0] < self.pts[12][0] and self.pts[0][0] < self.pts[16][0] and self.pts[0][0] < self.pts[20][0]:
                 ch1 = 3
 
-        # con for [gh][l]
         l = [[4, 6], [4, 1], [4, 5], [4, 3], [4, 7]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[4][0] > self.pts[0][0]:
                 ch1 = 3
 
-        # con for [gh][pqz]
         l = [[5, 3], [5, 0], [5, 7], [5, 4], [5, 2], [5, 1], [5, 5]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[2][1] + 15 < self.pts[16][1]:
                 ch1 = 3
 
-        # con for [l][x]
         l = [[6, 4], [6, 1], [6, 2]]
         pl = [ch1, ch2]
         if pl in l:
             if self.distance(self.pts[4], self.pts[11]) > 55:
                 ch1 = 4
 
-        # con for [l][d]
         l = [[1, 4], [1, 6], [1, 1]]
         pl = [ch1, ch2]
         if pl in l:
@@ -218,21 +273,18 @@ class SignPredictor:
                     self.pts[20][1]):
                 ch1 = 4
 
-        # con for [l][gh]
         l = [[3, 6], [3, 4]]
         pl = [ch1, ch2]
         if pl in l:
             if (self.pts[4][0] < self.pts[0][0]):
                 ch1 = 4
 
-        # con for [l][c0]
         l = [[2, 2], [2, 5], [2, 4]]
         pl = [ch1, ch2]
         if pl in l:
             if (self.pts[1][0] < self.pts[12][0]):
                 ch1 = 4
 
-        # con for [gh][z]
         l = [[3, 6], [3, 5], [3, 4]]
         pl = [ch1, ch2]
         if pl in l:
@@ -240,7 +292,6 @@ class SignPredictor:
                 1]) and self.pts[4][1] > self.pts[10][1]:
                 ch1 = 5
 
-        # con for [gh][pq]
         l = [[3, 2], [3, 1], [3, 6]]
         pl = [ch1, ch2]
         if pl in l:
@@ -248,77 +299,66 @@ class SignPredictor:
                 1] + 17 > self.pts[20][1]:
                 ch1 = 5
 
-        # con for [l][pqz]
         l = [[4, 4], [4, 5], [4, 2], [7, 5], [7, 6], [7, 0]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[4][0] > self.pts[0][0]:
                 ch1 = 5
 
-        # con for [pqz][aemnst]
         l = [[0, 2], [0, 6], [0, 1], [0, 5], [0, 0], [0, 7], [0, 4], [0, 3], [2, 7]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[0][0] < self.pts[8][0] and self.pts[0][0] < self.pts[12][0] and self.pts[0][0] < self.pts[16][0] and self.pts[0][0] < self.pts[20][0]:
                 ch1 = 5
 
-        # con for [pqz][yj]
         l = [[5, 7], [5, 2], [5, 6]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[3][0] < self.pts[0][0]:
                 ch1 = 7
 
-        # con for [l][yj]
         l = [[4, 6], [4, 2], [4, 4], [4, 1], [4, 5], [4, 7]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[6][1] < self.pts[8][1]:
                 ch1 = 7
 
-        # con for [x][yj]
         l = [[6, 7], [0, 7], [0, 1], [0, 0], [6, 4], [6, 6], [6, 5], [6, 1]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[18][1] > self.pts[20][1]:
                 ch1 = 7
 
-        # condition for [x][aemnst]
         l = [[0, 4], [0, 2], [0, 3], [0, 1], [0, 6]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[5][0] > self.pts[16][0]:
                 ch1 = 6
 
-        # condition for [yj][x]
         l = [[7, 2]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[18][1] < self.pts[20][1] and self.pts[8][1] < self.pts[10][1]:
                 ch1 = 6
 
-        # condition for [c0][x]
         l = [[2, 1], [2, 2], [2, 6], [2, 7], [2, 0]]
         pl = [ch1, ch2]
         if pl in l:
             if self.distance(self.pts[8], self.pts[16]) > 50:
                 ch1 = 6
 
-        # con for [l][x]
         l = [[4, 6], [4, 2], [4, 1], [4, 4]]
         pl = [ch1, ch2]
         if pl in l:
             if self.distance(self.pts[4], self.pts[11]) < 60:
                 ch1 = 6
 
-        # con for [x][d]
         l = [[1, 4], [1, 6], [1, 0], [1, 2]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[5][0] - self.pts[4][0] - 15 > 0:
                 ch1 = 6
 
-        # con for [b][pqz]
         l = [[5, 0], [5, 1], [5, 4], [5, 5], [5, 6], [6, 1], [7, 6], [0, 2], [7, 1], [7, 4], [6, 6], [7, 2], [5, 0],
              [6, 3], [6, 4], [7, 5], [7, 2]]
         pl = [ch1, ch2]
@@ -327,7 +367,6 @@ class SignPredictor:
                 1]):
                 ch1 = 1
 
-        # con for [f][pqz]
         l = [[6, 1], [6, 0], [0, 3], [6, 4], [2, 2], [0, 6], [6, 2], [7, 6], [4, 6], [4, 1], [4, 2], [0, 2], [7, 1],
              [7, 4], [6, 6], [7, 2], [7, 5], [7, 2]]
         pl = [ch1, ch2]
@@ -343,7 +382,6 @@ class SignPredictor:
                     self.pts[18][1] > self.pts[20][1]):
                 ch1 = 1
 
-        # con for [d][pqz]
         l = [[5, 0], [3, 4], [3, 0], [3, 1], [3, 5], [5, 5], [5, 4], [5, 1], [7, 6]]
         pl = [ch1, ch2]
         if pl in l:
@@ -372,7 +410,6 @@ class SignPredictor:
             if self.pts[5][0] - self.pts[4][0] - 15 < 0:
                 ch1 = 1
 
-        # con for [i][pqz]
         l = [[5, 4], [5, 5], [5, 1], [0, 3], [0, 7], [5, 0], [0, 2], [6, 2], [7, 5], [7, 1], [7, 6], [7, 7]]
         pl = [ch1, ch2]
         if pl in l:
@@ -380,7 +417,6 @@ class SignPredictor:
                  self.pts[18][1] > self.pts[20][1])):
                 ch1 = 1
 
-        # con for [yj][bfdi]
         l = [[1, 5], [1, 7], [1, 1], [1, 6], [1, 3], [1, 0]]
         pl = [ch1, ch2]
         if pl in l:
@@ -389,7 +425,6 @@ class SignPredictor:
              self.pts[18][1] > self.pts[20][1])):
                 ch1 = 7
 
-        # con for [uvr]
         l = [[5, 5], [5, 0], [5, 4], [5, 1], [4, 6], [4, 1], [7, 6], [3, 0], [3, 5]]
         pl = [ch1, ch2]
         if pl in l:
@@ -397,7 +432,6 @@ class SignPredictor:
                  self.pts[18][1] < self.pts[20][1])) and self.pts[4][1] > self.pts[14][1]:
                 ch1 = 1
 
-        # con for [w]
         fg = 13
         l = [[3, 5], [3, 0], [3, 6], [5, 1], [4, 1], [2, 0], [5, 0], [5, 5]]
         pl = [ch1, ch2]
@@ -408,14 +442,12 @@ class SignPredictor:
                 0]) and self.distance(self.pts[4], self.pts[11]) < 50:
                 ch1 = 1
 
-        # con for [w]
         l = [[5, 0], [5, 5], [0, 1]]
         pl = [ch1, ch2]
         if pl in l:
             if self.pts[6][1] > self.pts[8][1] and self.pts[10][1] > self.pts[12][1] and self.pts[14][1] > self.pts[16][1]:
                 ch1 = 1
 
-        # -------------------------condn for subgroups  starts
         if ch1 == 0:
             ch1 = 'S'
             if self.pts[4][0] < self.pts[6][0] and self.pts[4][0] < self.pts[10][0] and self.pts[4][0] < self.pts[14][0] and self.pts[4][0] < self.pts[18][0]:
@@ -500,16 +532,13 @@ class SignPredictor:
             if (self.pts[6][1] > self.pts[8][1] and self.pts[10][1] < self.pts[12][1] and self.pts[14][1] < self.pts[16][1] and self.pts[18][1] > self.pts[20][1]):
                 ch1=" "
 
-
         if ch1 == 'E' or ch1=='Y' or ch1=='B':
             if (self.pts[4][0] < self.pts[5][0]) and (self.pts[6][1] > self.pts[8][1] and self.pts[10][1] > self.pts[12][1] and self.pts[14][1] > self.pts[16][1] and self.pts[18][1] > self.pts[20][1]):
                 ch1="next"
 
-
         if ch1 == 'Next' or 'B' or 'C' or 'H' or 'F' or 'X':
             if (self.pts[0][0] > self.pts[8][0] and self.pts[0][0] > self.pts[12][0] and self.pts[0][0] > self.pts[16][0] and self.pts[0][0] > self.pts[20][0]) and (self.pts[4][1] < self.pts[8][1] and self.pts[4][1] < self.pts[12][1] and self.pts[4][1] < self.pts[16][1] and self.pts[4][1] < self.pts[20][1]) and (self.pts[4][1] < self.pts[6][1] and self.pts[4][1] < self.pts[10][1] and self.pts[4][1] < self.pts[14][1] and self.pts[4][1] < self.pts[18][1]):
                 ch1 = 'Backspace'
-
 
         if ch1=="next" and self.prev_char!="next":
             if self.ten_prev_char[(self.count-2)%10]!="next":
@@ -522,7 +551,6 @@ class SignPredictor:
                 if self.ten_prev_char[(self.count - 0) % 10] != "Backspace":
                     self.str = self.str + self.ten_prev_char[(self.count - 0) % 10]
 
-
         if ch1=="  " and self.prev_char!="  ":
             self.str = self.str + "  "
 
@@ -531,15 +559,12 @@ class SignPredictor:
         self.count += 1
         self.ten_prev_char[self.count%10]=ch1
 
-
         if len(self.str.strip()) != 0:
             st = self.str.rfind(" ")
             ed = len(self.str)
             word = self.str[st + 1:ed]
             self.word = word
             if len(word.strip()) != 0:
-                # CHANGED: pyspellchecker logic
-                # candidates returns a set, convert to list
                 cands = self.spell.candidates(word)
                 suggestions = list(cands) if cands else []
                 
@@ -559,7 +584,6 @@ class SignPredictor:
                 self.word4 = " "
 
     def _update_sentence(self):
-        # Kept the exact same update logic
         if self.current_symbol == "next" and self.prev_char != "next":
             if self.ten_prev_char[(self.count - 2) % 10] != "next":
                 if self.ten_prev_char[(self.count - 2) % 10] == "Backspace":
@@ -577,7 +601,6 @@ class SignPredictor:
             word = self.str[st+1:ed]
             self.word = word
             if len(word.strip()) != 0:
-                # CHANGED: pyspellchecker logic
                 cands = self.spell.candidates(word)
                 suggestions = list(cands) if cands else []
                 self.word1 = suggestions[0] if len(suggestions) > 0 else " "
@@ -593,14 +616,12 @@ class SignPredictor:
 
     def apply_suggestion(self, new_sentence):
         self.str = new_sentence.strip()
-        self.manual_override = True  # skip _update_sentence once
-        # Regenerate suggestions
+        self.manual_override = True
         if self.str:
             st = self.str.rfind(" ")
             word = self.str[st + 1:]
             self.word = word
             if word.strip():
-                # CHANGED: pyspellchecker logic
                 cands = self.spell.candidates(word)
                 suggestions = list(cands) if cands else []
                 self.word1 = suggestions[0] if len(suggestions) > 0 else " "
--- ikamai_prj/ikamai_app/utils.py
+++ ikamai_prj/ikamai_app/utils.py
@@ -1,43 +1,48 @@
-# myapp/utils.py
 import cv2
 import numpy as np
-import tensorflow as tf # CHANGED: Import TensorFlow for TFLite
+
+try:
+    from tflite_runtime.interpreter import Interpreter
+except ImportError:
+    import tensorflow.lite as tflite
+    Interpreter = tflite.Interpreter
+
 import mediapipe as mp
 import base64
 import io
 from PIL import Image
 
 class WordPredictor:
-    # CHANGED: Updated arguments to accept a single .tflite file instead of json/h5
     def __init__(self, model_path="model.tflite", threshold=0.8):
-        
-        # CHANGED: Load TFLite model and allocate tensors
-        self.interpreter = tf.lite.Interpreter(model_path=model_path)
+        self.interpreter = Interpreter(model_path=model_path)
         self.interpreter.allocate_tensors()
 
-        # Get input and output details
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
 
-        # Labels
         self.actions = np.array([
             'hello', 'yes', 'no', 'thanks',
             'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
             'eat', 'wrong', 'sorry', 'like', 'iloveyou','ihateyou', 'eat'
         ])
 
-        # State variables
         self.threshold = threshold
-        self.sequence = []      # last 30 frames of keypoints
-        self.sentence = []      # recognized words
-        self.accuracy = []      # confidence values
-        self.predictions = []   # raw predictions (indices)
+        self.sequence = []
+        self.sentence = []
+        self.accuracy = []
+        self.predictions = []
 
-        # Mediapipe setup
         self.mp_hands = mp.solutions.hands
+        
+        # --- FIX: Initialize Hands HERE, not in the loop ---
+        self.hands = self.mp_hands.Hands(
+            model_complexity=0, # Keep 0 for speed on server
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5
+        )
+        # ---------------------------------------------------
 
     def reset(self):
-        """Reset all state."""
         self.sequence = []
         self.sentence = []
         self.accuracy = []
@@ -45,14 +50,12 @@ class WordPredictor:
 
     def extract_keypoints(self, results):
         if results.multi_hand_landmarks:
-            # take only the first detected hand
             hand = results.multi_hand_landmarks[0]
             return np.array([[res.x, res.y, res.z] for res in hand.landmark]).flatten()
         else:
-            return np.zeros(21 * 3)  # 21 landmarks * 3 coordinates
+            return np.zeros(21 * 3)
 
     def mediapipe_detection(self, image, model):
-        """Process frame through Mediapipe hand model."""
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image.flags.writeable = False
         results = model.process(image)
@@ -61,76 +64,47 @@ class WordPredictor:
         return image, results
 
     def process_web_frame(self, image_data_base64):
-        """
-        Receives base64 image from frontend, processes it, updates sequence,
-        and returns prediction status.
-        """
         try:
-            # 1. Decode base64 image
             img_str = image_data_base64.split(',')[1]
             decoded = base64.b64decode(img_str)
             image = Image.open(io.BytesIO(decoded))
             frame = np.array(image)
             frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
 
-            # 2. Apply the SAME cropping as your original code
-            # Note: Ensure the incoming frame is large enough. 
-            # If frontend sends 640x480, this crop works.
             cropframe = frame[40:400, 0:300]
-            
-            # Optional: Visual debug on server (not visible to user)
-            # cv2.rectangle(frame, (0, 40), (300, 400), 255, 2)
 
-            # 3. Process with Mediapipe
-            with self.mp_hands.Hands(
-                model_complexity=0,
-                min_detection_confidence=0.5,
-                min_tracking_confidence=0.5
-            ) as hands:
-                
-                image, results = self.mediapipe_detection(cropframe, hands)
-                
-                # 4. Extract Keypoints & Update Sequence
-                keypoints = self.extract_keypoints(results)
-                self.sequence.append(keypoints)
-                self.sequence = self.sequence[-30:] # Keep last 30
+            # --- FIX: Use the self.hands instance we created in __init__ ---
+            image, results = self.mediapipe_detection(cropframe, self.hands)
+            # ---------------------------------------------------------------
 
-                # 5. Prediction Logic
-                if len(self.sequence) == 30:
-                    # CHANGED: TFLite prediction logic
-                    
-                    # Prepare input data (TFLite usually requires float32)
-                    input_data = np.expand_dims(self.sequence, axis=0).astype(np.float32)
-                    
-                    # Set the input tensor
-                    self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
-                    
-                    # Run inference
-                    self.interpreter.invoke()
-                    
-                    # Get output tensor
-                    res = self.interpreter.get_tensor(self.output_details[0]['index'])[0]
+            keypoints = self.extract_keypoints(results)
+            self.sequence.append(keypoints)
+            self.sequence = self.sequence[-30:]
 
-                    self.predictions.append(np.argmax(res))
+            if len(self.sequence) == 30:
+                input_data = np.expand_dims(self.sequence, axis=0).astype(np.float32)
+                self.interpreter.set_tensor(self.input_details[0]['index'], input_data)
+                self.interpreter.invoke()
+                res = self.interpreter.get_tensor(self.output_details[0]['index'])[0]
 
-                    # Check consistency + confidence
-                    if np.unique(self.predictions[-15:])[0] == np.argmax(res):
-                        if res[np.argmax(res)] > self.threshold:
-                            pred_word = self.actions[np.argmax(res)]
-                            confidence = round(res[np.argmax(res)] * 100, 2)
+                self.predictions.append(np.argmax(res))
 
-                            if len(self.sentence) > 0:
-                                if pred_word != self.sentence[-1]:
-                                    self.sentence.append(pred_word)
-                                    self.accuracy.append(confidence)
-                            else:
+                if np.unique(self.predictions[-15:])[0] == np.argmax(res):
+                    if res[np.argmax(res)] > self.threshold:
+                        pred_word = self.actions[np.argmax(res)]
+                        confidence = round(res[np.argmax(res)] * 100, 2)
+
+                        if len(self.sentence) > 0:
+                            if pred_word != self.sentence[-1]:
                                 self.sentence.append(pred_word)
                                 self.accuracy.append(confidence)
+                        else:
+                            self.sentence.append(pred_word)
+                            self.accuracy.append(confidence)
 
-                    if len(self.sentence) > 1:
-                        # keep only the most recent
-                        self.sentence = self.sentence[-1:]
-                        self.accuracy = self.accuracy[-1:]
+                if len(self.sentence) > 1:
+                    self.sentence = self.sentence[-1:]
+                    self.accuracy = self.accuracy[-1:]
             
             return self.get_status()
 
@@ -139,7 +113,6 @@ class WordPredictor:
             return self.get_status()
 
     def get_status(self):
-        """Return the latest stable prediction + accuracy."""
         if self.sentence and self.accuracy:
             return {
                 'prediction': self.sentence[-1],
